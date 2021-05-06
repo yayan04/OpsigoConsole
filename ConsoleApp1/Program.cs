e98182b70421f075a5aa237779a4ab1c2dd58ebc
Yayan Yanuari <yayanyanuari04@gmail.com>
2021-05-06 13:43:18 +0700

    Program Pesan Rahasia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,14 +15,90 @@ namespace ConsoleApp1
 
                 Console.WriteLine("=== OUTPUT ===");
 
-                BilanganJumlah(input);
+                SecretMessage(input);
 
-                Console.WriteLine("===============");
+                //BilanganJumlah(input);
+
+                Console.WriteLine("\n==============");
                 Console.Write("Ulang ? [Y/N] ");
                 repeat = Console.ReadLine();
             }
             while (repeat == "Y" || repeat == "y");
             
+        }
+
+        private static void SecretMessage(string message)
+        {
+            int i, j, square, n;
+            char[,] matriks = new char[10, 10];
+            char[,] transpose = new char[10, 10];
+
+            square = Convert.ToInt32(Math.Ceiling(Math.Sqrt(message.Length)));
+
+            n = Convert.ToInt32(Math.Pow(square, 2)) - message.Length;
+
+            string star = message;
+
+            for (int p = 0; p < n; p++)
+            {
+                star = star.Insert(star.Length, "*");
+            }
+
+            char[] text = star.ToCharArray();
+
+            for (i = 0; i < square; i++)
+            {
+                for (j = 0; j < square; j++)
+                {
+                    if (i == 0 && j <= square - 1)
+                    {
+                        matriks[i, j] = text[(i * j) + j];
+                    }
+                    else if (i != 0)
+                    {
+                        matriks[i, j] = text[(i * square) + j];
+                    }
+                }
+            }
+
+            //Console.WriteLine("\nMatriks: ");
+            //for (i = 0; i < square; i++)
+            //{
+            //    for (j = 0; j < square; j++)
+            //    {
+            //        Console.Write(matriks[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            for (i = 0; i < square; i++)
+            {
+                for (j = 0; j < square; j++)
+                {
+                    transpose[i, j] = matriks[(square - 1) - j, i];
+                }
+            }
+
+            //Console.WriteLine("\nHasil Transpose Matriks: ");
+            //for (i = 0; i < square; i++)
+            //{
+            //    for (j = 0; j < square; j++)
+            //    {
+            //        Console.Write(transpose[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            for (i = 0; i < square; i++)
+            {
+                for (j = 0; j < square; j++)
+                {
+                    if (transpose[i, j] != '*')
+                    {
+                        Console.Write(transpose[i, j]);
+                    }
+                }
+            }
         }
 
         private static void BilanganJumlah(string input)
